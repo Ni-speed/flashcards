1cdda5afc75b56f987bcfb22cc1819fdc253d0f4
Ni-speed <railmir@gmail.com>
2023-09-28 15:53:17 +0500

    add: Logo & Button

--- src/components/ui/header/header.tsx
+++ src/components/ui/header/header.tsx
@@ -1,7 +1,21 @@
 import { ComponentPropsWithoutRef, FC } from 'react'
 
 import s from './header.module.scss'
+
+import { Logo } from '@/assets'
+import { Button } from '@/components'
 type HeaderProps = ComponentPropsWithoutRef<'header'>
 export const Header: FC<HeaderProps> = ({ className, ...rest }) => {
-  return <header className={`${s.header} ${className}`} {...rest} />
+  return (
+    <div className={`${s.header} ${className}`} {...rest}>
+      <Logo />
+      <Button
+        as={'a'}
+        href="https://www.youtube.com/watch?v=j7Wbb2Bcd9c&list=PLMH1EP4byJHRnHhGr4-madQs_PNRTGXft&index=2&t=912s"
+        variant={'primary'}
+      >
+        Sign in
+      </Button>
+    </div>
+  )
 }
