67c7c194b9d471c5a3d51851d0d748c144089a58
Ni-speed <railmir@gmail.com>
2023-09-29 14:56:04 +0500

    feat: RadioGroup component

---
+++ src/components/ui/radioGroup/radioGroup.tsx
@@ -0,0 +1,44 @@
+import { ComponentPropsWithoutRef, FC } from 'react'
+
+import * as RadixRadioGroup from '@radix-ui/react-radio-group'
+
+import s from './radioGroup.module.scss'
+
+import { Typography } from '@/components'
+
+type Option = {
+  label: string
+  value: string
+}
+
+export type RadioGroupProps = {
+  options?: Option[]
+  value?: string
+  onChange?: (e: any) => void
+  disabled?: boolean
+  name?: string
+} & ComponentPropsWithoutRef<'div'>
+export const RadioGroup: FC<RadioGroupProps> = ({ options, className, onChange, disabled }) => {
+  return (
+    <form>
+      <RadixRadioGroup.Root className={`${s.radioGroupRoot} ${className}`} onValueChange={onChange}>
+        {options?.map((el, index) => {
+          return (
+            <div className={s.itemGroup} key={index}>
+              <RadixRadioGroup.Item
+                className={s.radioGroupItem}
+                disabled={disabled}
+                value={el.value}
+              >
+                <RadixRadioGroup.Indicator className={s.radioGroupIndicator} />
+              </RadixRadioGroup.Item>
+              <label className={`${s.label} ${disabled ? s.labelDisabled : ''}`}>
+                <Typography variant={'body2'}>{el.value}</Typography>
+              </label>
+            </div>
+          )
+        })}
+      </RadixRadioGroup.Root>
+    </form>
+  )
+}
