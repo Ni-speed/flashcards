ad7fb45a242eeaa710039d2ad58f9c4f9c64ce13
Ni-speed <railmir@gmail.com>
2023-10-06 00:01:19 +0500

    refactor: added forwardRef to Select component

--- src/components/ui/select/select.tsx
+++ src/components/ui/select/select.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode } from 'react'
+import { ElementRef, FC, forwardRef, ReactNode } from 'react'
 
 import * as SelectRadix from '@radix-ui/react-select'
 
@@ -18,49 +18,46 @@ export type SelectProps = {
   required?: boolean
 }
 
-export const Select: FC<SelectProps> = ({
-  defaultValue,
-  options,
-  value,
-  onValueChange,
-  disabled,
-  required,
-  placeholder,
-  label,
-}) => {
-  return (
-    <Typography variant={'body1'} as={'label'}>
-      <Typography variant={'body2'} as={'span'} className={`${s.label} ${disabled && s.disabled}`}>
-        {label}
-      </Typography>
-      <SelectRadix.Root
-        defaultValue={defaultValue}
-        value={value}
-        onValueChange={onValueChange}
-        disabled={disabled}
-        required={required}
-      >
-        <SelectRadix.Trigger asChild className={s.trigger} tabIndex={0}>
-          <div>
-            <SelectRadix.Value placeholder={placeholder} />
-            <SelectRadix.Icon asChild className={s.icon}>
-              <Down />
-            </SelectRadix.Icon>
-          </div>
-        </SelectRadix.Trigger>
+export const Select: FC<SelectProps> = forwardRef<ElementRef<typeof SelectRadix.Root>, SelectProps>(
+  ({ defaultValue, options, value, onValueChange, disabled, required, placeholder, label }) => {
+    return (
+      <Typography variant={'body1'} as={'label'}>
+        <Typography
+          variant={'body2'}
+          as={'span'}
+          className={`${s.label} ${disabled && s.disabled}`}
+        >
+          {label}
+        </Typography>
+        <SelectRadix.Root
+          defaultValue={defaultValue}
+          value={value}
+          onValueChange={onValueChange}
+          disabled={disabled}
+          required={required}
+        >
+          <SelectRadix.Trigger asChild className={s.trigger} tabIndex={0}>
+            <div>
+              <SelectRadix.Value placeholder={placeholder} />
+              <SelectRadix.Icon asChild className={s.icon}>
+                <Down />
+              </SelectRadix.Icon>
+            </div>
+          </SelectRadix.Trigger>
 
-        <SelectRadix.Portal>
-          <SelectRadix.Content position={'popper'} className={s.content} sideOffset={-1}>
-            <SelectRadix.Viewport>
-              {options.map(el => (
-                <SelectRadix.Item key={el.value} value={el.value} className={s.item}>
-                  <SelectRadix.ItemText>{el.label}</SelectRadix.ItemText>
-                </SelectRadix.Item>
-              ))}
-            </SelectRadix.Viewport>
-          </SelectRadix.Content>
-        </SelectRadix.Portal>
-      </SelectRadix.Root>
-    </Typography>
-  )
-}
+          <SelectRadix.Portal>
+            <SelectRadix.Content position={'popper'} className={s.content} sideOffset={-1}>
+              <SelectRadix.Viewport>
+                {options.map(el => (
+                  <SelectRadix.Item key={el.value} value={el.value} className={s.item}>
+                    <SelectRadix.ItemText>{el.label}</SelectRadix.ItemText>
+                  </SelectRadix.Item>
+                ))}
+              </SelectRadix.Viewport>
+            </SelectRadix.Content>
+          </SelectRadix.Portal>
+        </SelectRadix.Root>
+      </Typography>
+    )
+  }
+)
